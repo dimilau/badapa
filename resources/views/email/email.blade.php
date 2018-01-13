68048a9799896ed725f46873fce82cd5f73a53ba
Low Sum Wai <lowsumwai@gmail.comwq>
2018-01-13 08:17:14 +0000

    Update email template

--- resources/views/email/email.blade.php
+++ resources/views/email/email.blade.php
@@ -1,5 +1,11 @@
-<h1>Click the Link To Verify Your Email</h1>
-
-Click the following link to verify your email
-
-{{ url('/verifyemail/'.$email_token)}}
+Welcome and thank you for registering with Bad Apple. To speed up the registration process kindly <strong>click the following link</strong> to verify your email:<br/>
+<br/>
+<a href="{{ url('/verifyemail/'.$email_token)}}">{{ url('/verifyemail/'.$email_token)}}</a><br/>
+<br/>
+Once your email has been verified, your Bad Apple account will be activated within 12 hours. You will receive a welcome email from our team once your Bad Apple account is activated.<br/>
+<br/>
+Thank You!<br/>
+<br/>
+Regards,<br/>
+<br/>
+Bad Apple Team
